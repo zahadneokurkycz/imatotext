d70ed1aad84c911bad0ff2469c77fc26e452f375
zahadneokurkycz <filip.belzik@email.cz>
2023-02-01 17:44:03 +0100

    Added <itt> element

--- package/imatotext.js
+++ package/imatotext.js
@@ -1,4 +1,13 @@
 //This script is made by zahadneokurkycz
-    function ittStart() {
-        
-    }
+
+object.onload = function(){
+	ittStart();
+};
+
+function ittStart() {
+	if (document.querySelectorAll('itt').length > 0) {
+		console.info('Found ' + document.querySelectorAll('itt').length + ' Elements')
+	} else {
+		console.warn('There is zero of <itt></itt> elements');
+	}
+}
