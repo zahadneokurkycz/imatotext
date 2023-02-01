824385c2f74024274769a0a80266d1781ca77c02
zahadneokurkycz <filip.belzik@email.cz>
2023-02-01 19:11:18 +0100

    Update imatotext.js

--- package/imatotext.js
+++ package/imatotext.js
@@ -6,8 +6,8 @@ document.body.onload = function(){
 
 function ittStart() {
 	if (document.querySelectorAll('itt').length > 0) {
-		console.info('Found ' + document.querySelectorAll('itt').length + ' Elements')
+		console.log('Found ' + document.querySelectorAll('itt').length + ' Elements')
 	} else {
-		console.warn('There is zero of <itt></itt> elements');
+		console.log('There is zero of <itt></itt> elements');
 	}
 }
