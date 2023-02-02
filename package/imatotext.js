05ec11c6a4892d937baf1d6702fca24fc3aac240
zahadneokurkycz <filip.belzik@email.cz>
2023-02-02 07:38:51 +0100

    Just more work

--- package/imatotext.js
+++ package/imatotext.js
@@ -13,7 +13,7 @@ function ittStart() {
 
 		for (let i = 0; i < itttag.length; i++) {
   			itttag[i].id = 'itt-' + i;
-			itttag[i].style = 'display: none;';
+			itttag[i].style = 'display: block;';
 			itttag[i].name = 'sus';
 		}
 
@@ -23,7 +23,24 @@ function ittStart() {
 }
 
 function ittConvert(id, imgurl, key) {
+	var canvs = document.createElement('canvas');
+	//<canvas id="myCanvas" width="750" height="500">your canvas loads here</canvas>
 	
+	canvs.id = 'itt-' + id + 'can';
+	canvs.width = 750;
+	canvs.height = 500;
+	canvs.innerHTML = 'Sorry but your browser does not support this';
+	document.getElementById('itt-' + id).appendChild(canvs);
+
+	var canvas = document.getElementById('itt-' + id + 'can');
+	var ctx = canvas.getContext('2d');
+
+	var myImage = new Image();
+	myImage.src = 'https://images.unsplash.com/photo-1572800578930-fd1013b506c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80';
+	myImage.onload = function(){
+		ctx.drawImage(myImage,0,0,canvas.width,canvas.height);
+	};
+
 }
 
 function ittUniConv(text) {
